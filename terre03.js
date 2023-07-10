c92620843cc65509c4d3d3b3527ad7799153b236
Aldo FWI <aldo.fwi@gmail.com>
2023-07-10 18:00:03 -0400

    Alphabet

--- terre03.js
+++ terre03.js
@@ -3,10 +3,14 @@ let result = "";
 
 let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
 
-for(let i=0; i<alphabet.length; i++){
+let character = process.argv.slice(2);
+
+for(let i=alphabet.indexOf(character); i<alphabet.length; i++){
 
     result += alphabet[i];
 
 }
 
-console.log(result+"\n");
+console.log(result);
+
+console.log("\n");
