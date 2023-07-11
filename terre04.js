c5146528b01526df53d4b4be8e73e849f0e27dde
Aldo FWI <aldo.fwi@gmail.com>
2023-07-10 23:40:41 -0400

    Pair Impair

--- terre04.js
+++ terre04.js
@@ -1,13 +1,23 @@
-
+let noargs = false;
 let msgerr = "Tu ne me la mettras pas à l'envers.";
 
 process.argv.forEach(function(val, index, array) { 
     
-    if (index > 1) {
+    if( array.length !== 3 ) {
+        
+        noargs = true;
 
-        val >= 0 && val%2 !== NaN ? isEven(val) : console.log(msgerr);
+    } else {
+
+        if (index > 1) {
+
+            val >= 0 && val%2 !== NaN ? isEven(val) : console.log(msgerr);
+        }
     }
+
 });
+
+if(noargs) console.log(msgerr);
 
 function isEven(num) {
 
