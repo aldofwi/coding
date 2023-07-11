d40298dbae5eaf85ff7ce82b8ca87b71d4a2db2c
Aldo FWI <aldo.fwi@gmail.com>
2023-07-11 15:37:10 -0400

    Puissance

---
+++ terre08.js
@@ -0,0 +1,31 @@
+
+/** PUISSANCE */
+
+let noargs = false;
+let msgerr = "erreur.";
+let arg1, arg2, result=1;
+
+process.argv.forEach(function(val, index, array) { 
+    
+    if( array.length !== 4 ) noargs = true;
+    else {
+
+        if (index === 2) { arg1 = val; }
+        if (index === 3) { arg2 = val; }
+    }
+});
+
+if( !noargs && !isNaN(arg1) && arg2 >= 0 ) {
+
+    if(arg2 !== '0') {
+        result = arg1;
+
+        for(let i=0; i<arg2-1; i++){
+            result *= arg1;
+        }
+    }
+
+    console.log(result);
+} else {
+    console.log(msgerr);
+}
