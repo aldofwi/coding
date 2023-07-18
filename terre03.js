f38585dcc53195b2b8d20ffe37485b51973ace88
Aldo FWI <aldo.fwi@gmail.com>
2023-07-18 17:44:37 -0400

    Alphabet Index

--- terre03.js
+++ terre03.js
@@ -4,16 +4,43 @@
 let result = "";
 
 let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+let id = 0;
+let noargs = false;
+let msgerr = "erreur.";
 
-let character = process.argv.slice(2)[0];
+process.argv.forEach(function(val, index, array) { 
+
+if( array.length !== 3 ) {
+    noargs = true;
+} else {
+
+    let character = process.argv.slice(2)[0];
+
+    if(index === 2) {
+        console.log(character);
+
+        while(id <= alphabet.length) {
+            if(val !== alphabet[id]) noargs = true
+            else {
+                noargs = false; break
+            }
+            id++;
+        }
+
+        console.log(noargs);
+
+        if(!noargs) {
+            for(let i=alphabet.indexOf(character); i<alphabet.length; i++){
+                result += alphabet[i];
+            }
+        }
+    }
+}
+});
+
+if(noargs) console.warn(msgerr)
+else console.log(result+"\n");
+
 
 // console.log(character);
 // console.log(alphabet.indexOf(character));
-
-for(let i=alphabet.indexOf(character); i<alphabet.length; i++){
-
-    result += alphabet[i];
-
-}
-
-console.log(result+"\n");
