f68922b1e6fd7490c2863813e4410a37ef17c886
Aldo FWI <aldo.fwi@gmail.com>
2023-07-20 19:59:57 -0400

    Inversion

--- Terre/terre06.js
+++ Terre/terre06.js
@@ -10,9 +10,7 @@ process.argv.forEach(function(val, index, array) {
     if( array.length !== 3 ) {
         noargs = true;
     } else {
-        
         if (index === 2) {
-
             for(let i=val.length-1; i>=0; i--) {
                 chaine += val.charAt(i);
             }
