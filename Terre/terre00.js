2115beeaa929668a7d4b7df00702c1df11568df2
Aldo FWI <aldo.fwi@gmail.com>
2023-07-20 19:54:20 -0400

    Alphabet

--- Terre/terre00.js
+++ Terre/terre00.js
@@ -2,13 +2,10 @@
 /** ALPHABET */
 
 let result = "";
-
 let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
 
 for(let i=0; i<alphabet.length; i++){
-
     result += alphabet[i];
-
 }
 
 console.log(result+"\n");
