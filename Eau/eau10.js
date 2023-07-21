c845d077d4d62bcbb71ba5b1aff558cff2b40499
Aldo FWI <aldo.fwi@gmail.com>
2023-07-21 00:07:52 -0400

    Index Wanted

--- Eau/eau10.js
+++ Eau/eau10.js
@@ -1,7 +1,35 @@
 
-/** TITLE */
+/** Index Wanted */
+
+let noargs = false;
+let msgerr = "error.";
+let current= ""; let result = -1;
+
 // FUNCTIONS
+checkArgs = (args) => {
+    if(args.length < 2) noargs = true;
+    else {
+        return args[args.length-1];
+    }
+}
+
+foundElements = () => {
+
+    let index;
+    for(let a=0; a<myArgs.length-1; a++) {
+        if(myArgs[a] === lastArg) return a;
+        else index = -1;
+    }
+    if(index === -1) return index;
+}
+
 // ERRORS
 // PARSING
+let myArgs = process.argv.slice(2);
+let lastArg = checkArgs(myArgs);
+result = foundElements(lastArg);
+
 // RESULT
 // DISPLAY
+if(noargs) console.warn(msgerr);
+else console.log(result);
