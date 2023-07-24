b165db8edc8b10d0ef450e9d2c1f58293186243b
Aldo FWI <aldo.fwi@gmail.com>
2023-07-24 18:51:24 -0400

    Terre : OK

--- Terre/terre14.js
+++ Terre/terre14.js
@@ -5,6 +5,6 @@ let adj = ["cool", "sympa", "super", "bien", "intéressant"];
 
 let min = 0;
 let max = adj.length-1;
-let index = Math.floor(Math.random() * (max - min +1 ) + min);
+let index = Math.floor( Math.random() * (max - min +1 ) + min );
 
 console.log("J'ai terminé l'épreuve de la Terre et c'était "+ adj[index] +".");
