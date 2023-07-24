df94c0439c8f3ff0d22f5ed932b5a3684b66950d
Aldo FWI <aldo.fwi@gmail.com>
2023-07-24 18:44:39 -0400

    Nombre Premier

--- Terre/terre10.js
+++ Terre/terre10.js
@@ -3,6 +3,7 @@
 
 let noargs = false;
 let msgerr = "erreur.";
+let premier = false;
 let nombre;
 
 process.argv.forEach(function(val, index, array) { 
@@ -17,10 +18,29 @@ process.argv.forEach(function(val, index, array) {
 });
 
 if (!noargs && !isNaN(nombre)) {
+    if(nombre === 0 || nombre === 1)        premier = false;
+    else if(nombre === 2 || nombre === 3)   premier = true;
+    else {
+        premier = true;
+        for(let i=2; i<=7; i++) {
+            if(nombre !== i && nombre%i == 0)   premier = false;
+        }
+    }
+
+} else {
+    noargs = true;
+}
+
+if(noargs) console.log(msgerr);
+else if(premier) console.log("Oui, "+ nombre+" est un nombre premier.");
+else console.log("Non, "+ nombre+" n'est pas un nombre premier.");
+
+/*
+
+if (!noargs && !isNaN(nombre)) {
     if(nombre === 2 || nombre === 3) {
         console.log("Oui, "+ nombre+" est un nombre premier.");
-    } else if(  nombre === 0 || nombre === 1 || nombre%2 === 0 || 
-                nombre%3 === 0 || nombre%5 === 0 || nombre%7 === 0) {
+    } else if(  nombre === 0 || nombre === 1 || nombre%2 === 0 || nombre%3 === 0 || nombre%5 === 0 || nombre%7 === 0) {
         console.log("Non, "+ nombre+" n'est pas un nombre premier.");
     } else {
         console.log("Oui, "+ nombre+" est un nombre premier.");
@@ -28,3 +48,5 @@ if (!noargs && !isNaN(nombre)) {
 } else {
     console.log(msgerr);
 }
+
+*/
