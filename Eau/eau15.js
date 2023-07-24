4195571bbb3ee8924b26b7d48ad9cbcb90531bfe
Aldo FWI <aldo.fwi@gmail.com>
2023-07-24 18:50:15 -0400

    Eau : OK

--- Eau/eau15.js
+++ Eau/eau15.js
@@ -2,16 +2,14 @@
 /** Eau : OK */
 
 let adj = ["instructif", "pas mal", "utile", "très logique", "inspirant"];
-// FUNCTIONS
-// ERRORS
-// PARSING
-// RESULT
-// DISPLAY
 let min = 0;
 let max = adj.length-1;
 // let min = Math.ceil(0);
 //let max = Math.floor(adj.length);
 let index = Math.floor(Math.random() * (max - min +1 ) + min);
-console.log(index);
-
 console.log("J'ai terminé l'épreuve de l'Eau et c'était "+ adj[index] +".");
+// FUNCTIONS
+// ERRORS
+// PARSING
+// RESULT
+// DISPLAY
