646e5a0de0d0ada0b9cd2fab117c0235b89f6d9c
Aldo FWI <aldo.fwi@gmail.com>
2023-07-26 19:49:42 -0400

    Insertion dans tri

---
+++ Air/air07.js
@@ -0,0 +1,49 @@
+
+/** Insertion dans un tableau trié */
+
+const msgerr = "error.";
+let noargs = false;
+let tableau = [];
+let insert = -1;
+let result = "";
+let done = false;
+
+// FUNCTIONS
+validateArgs = (args) => { 
+    if(args.length < 3) return true;
+
+    for(let z=1; z<args.length-1; z++) {
+        if(isNaN(args[z-1]) || args[z] < args[z-1]) return true;
+    }
+    insert = +args[args.length-1];
+}
+
+insertElement = (param, element) => {
+
+    for(let d=0; d<param.length-1; d++) {
+        if(element < +param[d] && !done) {
+            tableau.push(element);
+            done = true;
+        }
+        tableau.push(+param[d]);
+    }
+    return tableau;
+}
+
+displayTab = (tableau) => {
+
+    for(let d=0; d<tableau.length; d++) {
+        result += tableau[d] + " ";
+    }
+    console.log(result);
+}
+
+// ERRORS
+// PARSING
+let arg = process.argv.slice(2); 
+noargs = validateArgs(arg);
+
+// RESULT
+// DISPLAY
+if(noargs) console.warn(msgerr);
+else displayTab(insertElement(arg, insert));
