ee957af382670510ea7f2f1a1246d38de6c12911
Aldo FWI <aldo.fwi@gmail.com>
2023-08-07 21:48:50 -0400

    META

--- Air/air13.js
+++ Air/air13.js
@@ -4,17 +4,35 @@
 const {exec} = require("child_process");
 const fs = require("fs");
 const msgerr = "error.";
+const extension = ".js";
 let noargs = false;
+let myParamsEntry = [
+    ["Bonjour les gars"],
+    ["Bonjour les gars", "les"],
+    ["Everybody", "wants", "a", "headline", " "],
+    ["1 2 3 4 5 4 3 2 1"],
+    ["Hiii Guyyyz,     wwwwhats uuup ??"],
+    ["10 11 12 20 -5"],
+    ["Michel", "Albert", "Thérèse", "Benoît", "t"],
+    ["10 20 30 40 50 60 70 90 35"],
+    ["10 20 30 fusion 15 25 35"],
+    ["Michel", "Albert", "Thérèse", "Benoît"],
+    ["a.txt"],
+    ["A 5"],
+    ["9 5 8 7 3 1 4 2"],
+];
+
+let myParamsSorty = [
+    ["Bonjour\nles\ngars\n"],
+    ["Bonjour \ngars\n"],
+    ["Everybody wants a headline \n"],
+    ["5 \n"],
+];
 
 let current = process.argv.slice(1)[0];
-let currentFile = current.slice(current.length-8, current.length);
-let currentFolder = current.slice(0, current.length-8);
-let currentCall = current.slice(current.length-12, current.length);
 
 console.log("------------------------------");
-console.log("currentCall :", currentCall);
-console.log("currentFile :", currentFile);
-console.log("currentFolder :", currentFolder);
+console.log("|           META             |");
 console.log("------------------------------");
 
 // FUNCTIONS
@@ -24,6 +42,7 @@ checkPresence = () => {
 
     let title = "air0";
     let title1 = title.slice(0, title.length-1);
+    let currentFolder = current.slice(0, current.length-8);
 
     for(let i=0; i<13; i++) {
 
@@ -40,18 +59,95 @@ checkPresence = () => {
         }
     }
 
+}
+
+displayVersion = () => {
+
     exec('npm --version', (error, stdout, stderr) => {
         console.log("------------------------------");
         console.log(`npm --version ${stdout.trim()}`);
         console.log("------------------------------");
     });
-
-    launchScript();
-    
 }
 
-launchScript = () => {
-    
+launchScripts = () => {
+
+    let indice = "0";
+    let currentCall = current.slice(current.length-12, current.length);
+    let body = currentCall.slice(0, currentCall.length-5)+"01"+extension;
+    let liveParam = "";
+
+    console.log("TAILLE : ", myParamsEntry[2].length);
+
+    for(let i=3; i<4; i++) {
+
+        if(i > 9) indice = "";
+        body = currentCall.slice(0, currentCall.length-5)+indice+i+extension;
+
+        for(let j=0; j<myParamsEntry[i].length; j++) {
+            liveParam += `"`+myParamsEntry[i][j]+`"`;
+            if(j !== myParamsEntry[i].length-1) liveParam += " ";
+        }
+        
+        console.log("PARAMS : ", liveParam);
+        console.log("SORTY : ", myParamsSorty[3][0]);
+
+        exec(`node "${body}" "${liveParam}"`, (error, stdout, stderr) => {
+            console.log("------------------------------");
+            console.log(`Result ${body} :\n${stdout}`);
+            console.log("\n------------------------------");
+            
+            console.log(stdout);
+            for(let i=0; i<stdout.length; i++) { console.log(i+" "+stdout[i]); }
+            if(stdout === myParamsSorty[2][0]) console.log("SUCCESS");
+            
+            if(error) {
+                console.log(`Error Split : ${stderr.trim()}`);
+                console.log("\n------------------------------");
+            }
+        });
+    }
+}
+
+// ERRORS
+// PARSING
+let arg = process.argv.slice(2); 
+noargs = validateArgs(arg);
+
+// RESULT
+// DISPLAY
+if(noargs) console.warn(msgerr);
+else {
+    checkPresence();
+    launchScripts();
+    displayVersion();
+}
+
+
+/*
+
+let currentFile = current.slice(current.length-8, current.length);
+
+console.log("------------------------------");
+console.log("\x1b[31m currentCall : \x1b[0m", currentCall);
+console.log("currentFile :", currentFile);
+console.log("currentFolder :", currentFolder);
+console.log("------------------------------");
+
+exec('echo "They gave ME the KEY to the SKY." | cat > a.txt', (error, stdout, stderr) => {
+    console.log("------------------------------");
+    console.log(`${stdout.trim()}`);
+    console.log("------------------------------");
+});
+
+exec('node Air/air10.js a.txt', (error, stdout, stderr) => {
+    console.log("------------------------------");
+    console.log(`FILE : ${stdout.trim()}`);
+    console.log("------------------------------");
+});
+
+// Old launch script
+
     let extension = ".js";
     let body = currentCall.slice(0, currentCall.length-5)+"01"+extension;
     let param = "Bonjour les gars ";
@@ -65,14 +161,4 @@ launchScript = () => {
         console.log("\n------------------------------");
     });
 
-}
-
-// ERRORS
-// PARSING
-let arg = process.argv.slice(2); 
-noargs = validateArgs(arg);
-
-// RESULT
-// DISPLAY
-if(noargs) console.warn(msgerr);
-else checkPresence();
+*/
