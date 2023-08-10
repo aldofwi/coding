bc523aa3d3ea0e65692d5765f34ae7230e81fe29
Aldo FWI <aldo.fwi@gmail.com>
2023-08-10 18:41:46 -0400

    Air : OK

---
+++ Air/air14.js
@@ -0,0 +1,14 @@
+
+/** Air : OK */
+
+let adj = ["passionnant", "bénéfique", "prenant", "cool", "long"];
+let min = 0;
+let max = adj.length-1;
+
+let index = Math.floor(Math.random() * (max - min +1 ) + min);
+console.log("J'ai terminé l'épreuve de l'Air et c'était "+ adj[index] +".");
+// FUNCTIONS
+// ERRORS
+// PARSING
+// RESULT
+// DISPLAY
