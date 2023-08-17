bd093f8efa1726d1fd473de834a9d6cdab4a2936
Aldo FWI <aldo.fwi@gmail.com>
2023-08-17 18:20:09 -0400

    Evaluer une expression

--- Feu/feu01.js
+++ Feu/feu01.js
@@ -38,19 +38,54 @@ defineOp = (formule) => {
     console.log( calculate2Nb(3, '%', 2) );
 }
 
-searchParenthesis = (formule) => {
+// 4 + 21 * (1 - 2 / 2) + 38
 
+searchPriority = (formule) => {
+
+    let num1 = " ";
+    let num2 = " ";
+    let mark = 0;
     let indexIn = 0;
     let indexOut = 0;
-    let new_parenthesis = 0;
+    let new_parenthesis = "";
 
     for(let i=0; i<formule.length; i++) {
         if( formule.charAt(i) === "(" ) indexIn = i;
         else if ( formule.charAt(i) === ")" ) indexOut = i;
     }
 
-    
+    // Si on ne trouve pas de parenthèses
+    if(indexIn === 0 || indexOut === 0) {
+        for(let i=0; i<formule.length; i++) {
 
+            if(formule.charAt(i) === "*" && formule.charAt(i-1) === " ") {
+                mark = i-2;
+                while(formule.charAt(mark) !== " ") {
+                    mark--;
+                }
+                console.log("mark =", mark);
+                console.log("indexIn =", indexIn);
+                console.log("chiffre =", formule.charAt(mark+1));
+
+                for(let j=mark+1; j<i-1; j++) {
+                    num1 += formule.charAt(j);
+                }
+                
+                if(formule.charAt(i+1) === " ") mark = i+2;
+                
+                while(formule.charAt(mark) !== " ") {
+                    num2 += formule.charAt(mark);
+                    mark++;
+                }
+
+                console.log("num1 =", +num1);
+                console.log("num2 =", +num2);
+                console.log("Result = ", +num1 * +num2);
+
+            }
+        }
+    }
+    
     return new_parenthesis;
 }
 
@@ -59,7 +94,7 @@ searchParenthesis = (formule) => {
 // PARSING
 let arg = process.argv.slice(2); 
 noargs = validateArgs(arg);
-if(!noargs) defineOp(arg[0]);
+if(!noargs) searchPriority(arg[0]);
 
 // RESULT
 // DISPLAY
