319c7bc019d784e8d5cf128bb11a61641cacb327
Aldo FWI <aldo.fwi@gmail.com>
2023-08-18 14:49:45 -0400

    Evaluer une expression

--- Feu/feu01.js
+++ Feu/feu01.js
@@ -5,9 +5,10 @@ const msgerr = "error.";
 let noargs = false;
 let result = "";
 let nbOp = 0;
+let listOp = [];
 
 // FUNCTIONS
-validateArgs = (args) => { 
+validateArgs = (args) => {
     return (args.length < 1); 
 }
 
@@ -24,62 +25,75 @@ calculate2Nb = (nb1, operation, nb2) => {
 }
 
 // 4 + 21 * (1 - 2 / 2) + 38
-
+// Recherche de Parenthèses
 searchPriority = (formule) => {
 
-    let num1 = " ";
-    let num2 = " ";
-    let mark = 0;
-    let indexIn = 0;
-    let indexOut = 0;
-    let new_number = 0;
-    let new_parenthesis = "";
+    let indexOpen = 0;
+    let indexClosed = 0;
+    let parenthesis = false;
+    let new_formula = "";
 
     for(let i=0; i<formule.length; i++) {
-        if( formule.charAt(i) === "(" ) indexIn = i;
-        else if ( formule.charAt(i) === ")" ) indexOut = i;
-    }
-
-    // Si on ne trouve pas de parenthèses
-    if(indexIn === 0 || indexOut === 0) {
-        for(let i=0; i<formule.length; i++) {
-
-            if(formule.charAt(i) === "*" && formule.charAt(i-1) === " ") {
-                mark = i-2;
-                while(formule.charAt(mark) !== " ") {
-                    mark--;
-                }
-                console.log("mark =", mark);
-                console.log("chiffre =", formule.charAt(mark+1));
-
-                indexIn = mark+1;
-
-                for(let j=mark+1; j<i-1; j++) {
-                    num1 += formule.charAt(j);
-                }
-                console.log("indexIn =", indexIn);
-                
-                if(formule.charAt(i+1) === " ") mark = i+2;
-                
-                while(formule.charAt(mark) !== " ") {
-                    num2 += formule.charAt(mark);
-                    mark++;
-                    indexOut = mark;
-                }
-                console.log("indexOut =", indexOut);
-                new_number = +num1 * +num2;
-
-                console.log("num1 =", +num1);
-                console.log("num2 =", +num2);
-                console.log("Result = ", +num1 * +num2);
-                console.log("Slice =", formule.slice(indexIn, indexOut));
-                console.log("Formule =", formule.replace(formule.slice(indexIn, indexOut), new_number));
-
-            }
+        if( formule.charAt(i) === "(" ) indexOpen = i;
+        else if ( formule.charAt(i) === ")" ) {
+            indexClosed = i;
+            parenthesis = true;
         }
     }
     
-    return new_parenthesis;
+    console.log("indexOpen =", indexOpen);
+    console.log("indexClosed =", indexClosed);
+    console.log("searchPriority (formule) -->", formule);
+    new_formula = formule.slice(indexOpen+1, indexClosed);
+    console.log("searchPriority (new_formula) -->", new_formula);
+
+    if(parenthesis) {
+        calculatePriority(new_formula);
+    }
+
+}
+
+calculatePriority = (formule) => {
+
+    let num1 = " ";
+    let num2 = " ";
+    let operation = "";
+    let mark = 0;
+    let indexIn = 0;
+    let indexOut = 0;
+    
+    for(let i=0; i<formule.length; i++) {
+
+        if( formule.charAt(i) === "*" || formule.charAt(i) === "/") {
+            console.log("searchPriority (startIf) -->", formule);
+
+            mark = i-2;
+            operation = formule.charAt(i);
+            while(  formule.charAt(mark) !== " " && mark >= 0 ) {
+                mark--;
+            }
+            indexIn = mark+1;
+
+            for(let j=mark+1; j<i-1; j++) {
+                num1 += formule.charAt(j);
+            }
+            console.log("indexIn =", indexIn);
+            
+            if( formule.charAt(i+1) === " " ) mark = i+2;
+            
+            while(formule.charAt(mark) !== " " && mark < formule.length) {
+                num2 += formule.charAt(mark);
+                mark++;
+                indexOut = mark;
+            }
+            console.log("indexOut =", indexOut);
+
+            console.log("num1 =", +num1);
+            console.log("num2 =", +num2);
+            console.log("Slice =", formule.slice(indexIn, indexOut));
+            console.log("Formule =", formule.replace(formule.slice(indexIn, indexOut), calculate2Nb(+num1, operation, +num2)));
+        }
+    }
 }
 
 
