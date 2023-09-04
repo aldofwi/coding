1961509394c1ce776b2af534bcb3ef9adc3c5f6c
Aldo FWI <aldo.fwi@gmail.com>
2023-09-04 17:24:09 -0400

    Trouver la forme

--- Feu/feu02.js
+++ Feu/feu02.js
@@ -5,13 +5,29 @@ const fs = require("fs");
 const msgerr = "Introuvable.";
 let noargs = false;
 let result = "";
+let shape = [];
 
 // FUNCTIONS
 const validateArgs = (args) => {
-    return (args.length !== 2); 
+    return (args.length !== 2);
 }
 
 const saveShape = (fichier) => {
+
+    fs.readFile(fichier, 'utf8', (err, data) => {
+        if(err) console.warn(msgerr);
+
+        let j = 0;
+        for(let i=0; i<data.length; i++) {
+
+            while(data.charCodeAt(j) !== 10) {
+                shape.push([i,j]);
+                j++;
+            }
+            
+        }
+        console.log(shape);
+    });
 
 }
 
@@ -22,7 +38,7 @@ const foundShape = () => {
 
 const sendCoordonates = () => {
 
-} 
+}
 
 const displayFile = (fichier) => {
 
@@ -46,4 +62,8 @@ noargs = validateArgs(arg);
 // RESULT
 // DISPLAY
 if(noargs) console.warn(msgerr);
-else displayFile(arg[0]);
+else {
+    // displayFile(arg[0]);
+    displayFile(arg[1]);
+    saveShape(arg[1]);
+}
