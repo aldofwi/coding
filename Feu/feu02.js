346e5a2350dfa9e16a13c7e7905fdd0cb203ee36
Aldo FWI <aldo.fwi@gmail.com>
2023-09-05 16:06:40 -0400

    Trouver la forme

--- Feu/feu02.js
+++ Feu/feu02.js
@@ -6,12 +6,14 @@ const msgerr = "Introuvable.";
 let noargs = false;
 let result = "";
 let shape = [];
+let board = "";
 
 // FUNCTIONS
 const validateArgs = (args) => {
     return (args.length !== 2);
 }
 
+// Save position of shape points
 const saveShape = (fichier) => {
 
     fs.readFile(fichier, 'utf8', (err, data) => {
@@ -31,17 +33,13 @@ const saveShape = (fichier) => {
                 col++;
             }
         }
-        console.log(shape);
+        console.log("Shape --> ", shape);
     });
 
 }
 
-// Define Spacing searched
-const foundShape = () => {
-
-}
-
-const sendCoordonates = () => {
+// Display shape with dashes (-)
+const displayShape = () => {
 
 }
 
@@ -57,18 +55,38 @@ const displayFile = (fichier) => {
     });
 }
 
+// Search shape on the board : OFF
+const foundShape = (fichier) => {
+
+    let iCol = 0;
+    let iLine = 0;
+
+    fs.readFile(fichier, 'utf8', (err, data) => {
+        if(err) console.warn(msgerr);
+
+        for(let i=0; i<data.length; i++) {
+            
+            if(data.charCodeAt(i) == shape[0][0]) {
+                console.log("["+i+"]"," --> Eureka!");
+                return;
+            }
+            
+        }
+        // console.log(data);
+    });
+}
+
 // ERRORS
 // PARSING
 let arg = process.argv.slice(2);
 noargs = validateArgs(arg);
 
-//console.log(arg);
-
 // RESULT
 // DISPLAY
 if(noargs) console.warn(msgerr);
 else {
-    // displayFile(arg[0]);
-    displayFile(arg[1]);
+
+    displayFile(arg[0]);
     saveShape(arg[1]);
+    foundShape(arg[0]);
 }
