1f791efc12b4e6ea45f3f96b36444b0f05314786
Aldo FWI <aldo.fwi@gmail.com>
2023-09-29 01:58:22 -0400

    Labyrinthe

--- Feu/feu05.js
+++ Feu/feu05.js
@@ -6,11 +6,17 @@ const msgerr = "error.";
 
 let nbLines;
 let nbCol;
+let nbCar;
 let full;
 let empty;
 let road;
 let entry;
 let exit;
+let wayz = []; // 2D Tab
+
+let exitPoint;
+let eureka = false;
+let blocked = false;
 
 // FUNCTIONS
 const validateArgs = (args) => {
@@ -44,6 +50,7 @@ const mapValidity = (dataz) => {
     let caracterz = [full, empty, entry, exit, '\n'];
     // console.log(caracterz);
 
+    nbCar = nbCol+1;
     let j=0;
     let k=0;
     let tabLignes = [];
@@ -83,21 +90,192 @@ const foundShortWay = (fichier) => {
             console.warn(msgerr);
             return;
         }
-        console.log("\nNb Lignes =", nbLines, "\nNb Colonnes =", nbCol, "\nCaractère plein =", full, "\nCaractère vide =", empty, "\nCaractère chemin =", road, "\nCaractère entrée =", entry, "\nCaractère sortie =", exit);
-        console.log('\n'+data.slice(11));
-        
+                        let position = [];
         let iCol = 0;   let entryPos = [];
         let iLine = 0;  let exitPos  = [];
 
-        // Repérer les positions entrée & sortie.
-        for(let i=nbCol+1; i<data.length; i++) {
+        console.log("\nNb Lignes =", nbLines, "\nNb Colonnes =", nbCol, "\nCaractère plein =", full, "\nCaractère vide =", empty, "\nCaractère chemin =", road, "\nCaractère entrée =", entry, "\nCaractère sortie =", exit);
+        console.log('\n'+data.slice(nbCar));
 
-            if(data.charAt(i) === entry) console.log("entry position =", i);
-            else if(data.charAt(i) === exit) console.log("exit position =", i);
+        // Repérer les positions entrée & sortie.
+        for(let i=nbCar; i<data.length; i++) {
+            position = [iCol, iLine, i];
+            if(data.charAt(i) === entry)        entryPos.push(position);
+            else if(data.charAt(i) === exit)    exitPos.push(position);
+            
+            if(data.charCodeAt(i) === 10) {
+                iCol = 0; iLine++;
+            } else iCol++;
         }
 
-
+        console.log(entryPos.length, ". entry =", entryPos[0]); 
+        console.log(exitPos.length, ". exit =", exitPos[0]);
+        exitPoint = foundExitPoint(exitPos[0], data);
+        console.log("0. exitPoint =", exitPoint);
+        // console.log("\nWhere Can I go ?", whereCanIgo(entryPos[0], entryPos[0], data));
+        
+        let oneWay = tryToFindOut(entryPos[0], data);
     });
+}
+
+// Trouver le + court chemin pour atteindre la sortie.
+const tryToFindOut = (comeIn, map) => {
+
+    let lastPos = [];
+    let currentPos = [];
+    let currentWay = [];
+    let currentDir = [];
+
+    while(wayz.length !== 3) {
+
+        currentPos = comeIn;
+        currentDir = whereCanIgo(comeIn, currentPos, currentWay, map);
+    //while(!eureka) {
+        while(!blocked) {
+
+            if(currentDir.length > 0) {
+                lastPos = currentPos;
+                // currentDir possède plusieurs voies, ATTENTION.
+                currentPos = goThere(currentDir[0], lastPos);
+                currentWay.push(currentPos[2]);
+                //console.log("last pos --> ", lastPos);
+                //console.log("current pos --> ", currentPos);
+                console.log("current way --> ", currentWay);
+            } else {
+                console.log("--> ! NO DIRECTION !");
+                break;
+            }
+            currentDir = whereCanIgo(currentPos, lastPos, currentWay, map);
+            // console.log("current dir --> ", currentDir);
+        }
+        wayz.push(currentWay);
+        console.log("Eureka :", eureka, "\nBlocked :", blocked,"\n---------------");
+        blocked = false;
+        lastPos = [];
+        currentPos = [];
+        currentWay = [];
+        currentDir = [];
+    }
+  
+    for(let i=0; i<wayz.length; i++) {
+        drawThatWay(wayz[i], map);
+    }
+    console.log("wayz : ", wayz);
+}
+
+// Passer d'une position à une autre
+const goThere = (direction, lastPosition) => {
+
+    let curr = [];
+
+    switch(direction) {
+        case 'left' : curr = [lastPosition[0]-1, lastPosition[1], lastPosition[2]-1]; break;
+        case 'right': curr = [lastPosition[0]+1, lastPosition[1], lastPosition[2]+1]; break;
+        case 'up'   : curr = [lastPosition[0], lastPosition[1]-1, lastPosition[2]-nbCar]; break;
+        case 'left' : curr = [lastPosition[0]-1, lastPosition[1]+1, lastPosition[2]+nbCar]; break;
+            default : console.log("-- switch OFF !"); break;
+    }
+
+    console.log("-- goThere(", curr[2],");");
+    return curr;
+}
+
+// Tester la possibilité de déplacement
+const whereCanIgo = (position, lastPos, currWay, map) => {
+
+    let current = currWay;
+    let directions = [];
+    console.log("-- whereCanIgo();");
+
+    if(position[0] !== 0 && map.charAt(position[2]-1) === ' ' && lastPos[2] !== position[2]-1) {
+        if(position[2]-1 === exitPoint[2]) eureka = true;
+        else {
+            current.push(position[2]-1);
+            if(!didIgo(current)) directions.push('left');
+            current.pop();
+        }
+    }
+
+    if(position[0] !== 9 && map.charAt(position[2]+1) === ' ' && lastPos[2] !== position[2]+1) {
+        if(position[2]+1 === exitPoint[2]) eureka = true;
+        else {
+            current.push(position[2]+1);
+            if(!didIgo(current)) directions.push('right');
+            current.pop();
+        }
+    }
+
+    if(position[1] !== 0 && map.charAt(position[2]-nbCar) === ' ' && lastPos[2] !== position[2]-nbCar) {
+        if(position[2]-nbCar === exitPoint[2]) eureka = true;
+        else {
+            current.push(position[2]-nbCar);
+            if(!didIgo(current)) directions.push('up');
+            current.pop();
+        }
+    }
+
+    if(position[1] !== 9 && map.charAt(position[2]+nbCar) === ' ' && lastPos[2] !== position[2]+nbCar) {
+        if(position[2]+nbCar === exitPoint[2]) eureka = true;
+        else {
+            current.push(position[2]+nbCar);
+            if(!didIgo(current)) directions.push('down');
+            current.pop();
+        }
+    }
+
+    if(directions.length === 0) blocked = true;
+
+    return directions;
+}
+
+// Vérifier si on est déja passé par là
+const didIgo = (thatWay) => {
+
+    let done = false;
+
+    for(let j=0; j<wayz.length; j++) {
+        
+        for(let k=0; k<thatWay.length; k++) {
+
+            if(wayz[j].length === thatWay.length && wayz[j][k] === thatWay[k]) done = true;
+            else done = false; break;
+        }
+    }
+    console.log("-- didIgo()", thatWay, "done =", done);
+    return done;
+}
+
+// Trouver le point libre vers la sortie
+const foundExitPoint = (exitPosition, map) => {
+    // On part du principe qu'il n'y a qu'un point d'accès à la sortie
+    let res ;
+
+    if(     exitPosition[0] !== 0 && map.charAt(exitPosition[2]-1) === ' ') res = 'left';
+    else if(exitPosition[0] !== 9 && map.charAt(exitPosition[2]+1) === ' ') res = 'right';
+    else if(exitPosition[1] !== 0 && map.charAt(exitPosition[2]-nbCar) === ' ') res = 'up';
+    else if(exitPosition[1] !== 9 && map.charAt(exitPosition[2]+nbCar) === ' ') res = 'down';
+
+    switch(res) {
+
+        case 'left' : return [exitPosition[0]-1, exitPosition[1], exitPosition[2]-1];
+        case 'right': return [exitPosition[0]+1, exitPosition[1], exitPosition[2]+1];
+        case 'up'   : return [exitPosition[0], exitPosition[1]-1, exitPosition[2]-nbCar];
+        case 'down' : return [exitPosition[0], exitPosition[1]+1, exitPosition[2]+nbCar];
+    }
+
+}
+
+// Afficher le chemin passé en paramètre
+const drawThatWay = (thatWay, map) => {
+
+    let theMap = map;
+    console.log("draw that waY --> ", thatWay);
+
+    for(let i=0; i<thatWay.length; i++) {
+        theMap = theMap.substring(0, thatWay[i]) + road + theMap.substring(thatWay[i]+1);
+    }
+    // "\x1b[31m"+road+"\x1b[0m"
+    console.log('\n'+theMap.slice(11));
 }
 
 // ERRORS
