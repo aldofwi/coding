2f691ba4b0f9b7f7b34b3f4e0ac6cf90d7e70904
Aldo FWI <aldo.fwi@gmail.com>
2023-09-29 20:39:35 -0400

    Feu : OK

---
+++ Feu/feu06.js
@@ -0,0 +1,15 @@
+
+/** Feu : OK */
+
+let adj = ["intense", "addictif", "intéressant", "perturbant", "jouissif"];
+let min = 0;
+let max = adj.length-1;
+
+let index = Math.floor(Math.random() * (max - min +1 ) + min);
+console.log("J'ai terminé l'épreuve du Feu et c'était "+ adj[index] +"!");
+
+// FUNCTIONS
+// ERRORS
+// PARSING
+// RESULT
+// DISPLAY
