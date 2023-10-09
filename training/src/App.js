e8adb39afa232b4c87e66015f19ac2c5979b262e
Aldo FWI <aldo.fwi@gmail.com>
2023-10-09 12:41:38 -0400

    Code 2 Improve

---
+++ training/src/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <div className="main">
+        <div className="input-area">
+            Hello World!
+            <input />
+            <button>Submit</button>
+        </div>
+       
+        <div>Yaaaayyyy</div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
