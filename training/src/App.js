bba2c1180ec1b7f0bd81c19f110e08f508900c7d
Aldo FWI <aldo.fwi@gmail.com>
2023-10-10 18:14:46 -0400

    Array of string Array

--- training/src/App.js
+++ training/src/App.js
@@ -1,20 +1,41 @@
-import './App.css'
+// Given an array of strings arrays : [['hello', 'goodbye', 'hello', 'alpha'], ['alpha', 'bravo']]
+// Find all the words that only appear in a SINGLE array. For example, 
+// this would return ['hello', 'goodbye', 'bravo'].
 
-const App = () => {
+const array = [['hello', 'goodbye', 'hello', 'alpha'], ['hello', 'bravo']];
 
-  return (
-    <div className="App">
-      <div className="main">
-        <div className="input-area">
-            Hello World!
-            <input />
-            <button>Submit</button>
-        </div>
-       
-        <div>Yaaaayyyy</div>
-      </div>
-    </div>
-  )
+// Identify all the words.
+const getUniqueWords = (arrayStr) => {
+  let currentArray = [];
+
+  for (let i = 0; i < arrayStr.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      if(!currentArray.includes(arrayStr[i][j])) currentArray.push(arrayStr[i][j]);
+    }
+  }
+  //console.log(currentArray);
+  checkRepetition(currentArray);
 }
 
-export default App;
+const checkRepetition = (curr) => {
+
+  let num = 0;
+  let finalArray = [];
+
+  curr.forEach(element => {
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].includes(element)) num++;
+      console.log(element, " ", num);
+    }
+    if(num<=1) finalArray.push(element);
+    num=0;
+
+  });
+
+  console.log(finalArray);
+
+}
+
+
+getUniqueWords(array);
