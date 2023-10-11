2ad59fc99433389d058be37d0129eb1a20a268d3
Aldo FWI <aldo.fwi@gmail.com>
2023-10-11 00:51:37 -0400

    Unique

--- training/src/App.js
+++ training/src/App.js
@@ -1,41 +1,10 @@
-// Given an array of strings arrays : [['hello', 'goodbye', 'hello', 'alpha'], ['alpha', 'bravo']]
-// Find all the words that only appear in a SINGLE array. For example, 
-// this would return ['hello', 'goodbye', 'bravo'].
+//import react from 'react';
 
-const array = [['hello', 'goodbye', 'hello', 'alpha'], ['hello', 'bravo']];
+const App = () => {
 
-// Identify all the words.
-const getUniqueWords = (arrayStr) => {
-  let currentArray = [];
-
-  for (let i = 0; i < arrayStr.length; i++) {
-    for (let j = 0; j < array[i].length; j++) {
-      if(!currentArray.includes(arrayStr[i][j])) currentArray.push(arrayStr[i][j]);
-    }
-  }
-  //console.log(currentArray);
-  checkRepetition(currentArray);
+  return(
+    <h1 className='App'> Hello le World ! </h1>
+  );
 }
 
-const checkRepetition = (curr) => {
-
-  let num = 0;
-  let finalArray = [];
-
-  curr.forEach(element => {
-
-    for (let i = 0; i < array.length; i++) {
-      if (array[i].includes(element)) num++;
-      console.log(element, " ", num);
-    }
-    if(num<=1) finalArray.push(element);
-    num=0;
-
-  });
-
-  console.log(finalArray);
-
-}
-
-
-getUniqueWords(array);
+export default App;
